6f5aeaef32db014546d507b8089725830abbc1eb
Mwangimark <markcosmars2000@gmail.com>
2025-08-23 22:24:05 +0300

    added mpesa logic

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+
+from .views import SubscriptionStatusView
 from .api import SubscribeView
 from .callbacks import mpesa_callback
 
 urlpatterns = [
     path("billing/subscribe/", SubscribeView.as_view(), name="subscribe"),
-    path("callback/", mpesa_callback, name="mpesa-callback")
+    path("callback/", mpesa_callback, name="mpesa-callback"),
+    path("subscription-status/", SubscriptionStatusView.as_view(), name="subscription-status"),
+
 ]
