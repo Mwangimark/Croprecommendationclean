e6aa6dd833b57143646ec400968bdf61c7d4833e
Mwangimark <markcosmars2000@gmail.com>
2025-08-26 00:34:56 +0300

    updated urls.py

--- CropRecommendation/urls.py
+++ CropRecommendation/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path("api/", include("subscriptions.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
